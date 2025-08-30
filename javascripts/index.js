b5acdb3710c6a75bde47aa91374fd5845b99710c
Gurpreetsingh-cu <gurpreetsingh22553@gmail.com>
2025-08-31 00:57:53 +0530

    today work
    today work

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,33 +1,51 @@
 
 
-let itemcontainerElement = document.querySelector(".items-container");
 
-let innerHTML ='';
-items.forEach( item=> {
-    innerHTML += 
-     `<div class="item-container">
-    <img class ="item-image" src="${item.image}" alt="image one">
-    <div class="rating">
-       ${item.rating.stars}
-    //    <div class = "count"</div>
-    </div>
-    <div class="company-name">${item.company }n</div>
-    <div class="item-name">${item.item_name}</div>
-    <div class="price">
-        <span class="current-price">$${item.current_price}</span>
-        <br>
-        <span class="orignal price">${item.original_price}</span>
-        <br>
-        <span class="Discount">${item.discount_percentage}% Discount price </span>
-    </div>
-    <div class="btn-add-bag ">Add to bag</div>
+let bagItems = [];
 
+function addToBag(itemId) {
+    bagItems.push(itemId);
+    displayBagItemCount();
+}
 
-   </div>
-`
-})
+function displayBagItemCount() {
+    const bagItemCountElement = document.querySelector(".bag-item-count");
 
+    if (bagItems.length > 0) {
+        bagItemCountElement.innerText = bagItems.length;
+        bagItemCountElement.style.visibility = "visible";
+    } else {
+        bagItemCountElement.style.visibility = "hidden";
+    }
+}
 
+function homePageDisplayItem() {
+    const itemContainerElement = document.querySelector(".items-container");
+    let innerHTML = '';
 
+    items.forEach(item => {
+        innerHTML += `
+            <div class="item-container">
+                <img class="item-image" src="${item.image}" alt="image one">
+                <div class="rating">
+                    ${item.rating.stars}
+                    <div class="count">${item.rating.count} reviews</div>
+                </div>
+                <div class="company-name">${item.company}</div>
+                <div class="item-name">${item.item_name}</div>
+                <div class="price">
+                    <span class="current-price">$${item.current_price}</span><br>
+                    <span class="original-price">$${item.original_price}</span><br>
+                    <span class="discount">${item.discount_percentage}% Discount</span>
+                </div>
+                <div class="btn-add-bag" onclick="addToBag(${item.id})">Add to bag</div>
+            </div>
+        `;
+    });
 
-itemcontainerElement.innerHTML = innerHTML
+    itemContainerElement.innerHTML = innerHTML;
+}
+
+// Initial render
+homePageDisplayItem();
+displayBagItemCount();
