5e176c0a3e99b08beee82ffa3d3bbbb37782a39d
Gurpreetsingh-cu <gurpreetsingh22553@gmail.com>
2025-08-31 00:53:15 +0530

    today work

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,40 +1,33 @@
 
 
-let itemcontainerElement = document.querySelector(".item-container");
+let itemcontainerElement = document.querySelector(".items-container");
+
+let innerHTML ='';
+items.forEach( item=> {
+    innerHTML += 
+     `<div class="item-container">
+    <img class ="item-image" src="${item.image}" alt="image one">
+    <div class="rating">
+       ${item.rating.stars}
+    //    <div class = "count"</div>
+    </div>
+    <div class="company-name">${item.company }n</div>
+    <div class="item-name">${item.item_name}</div>
+    <div class="price">
+        <span class="current-price">$${item.current_price}</span>
+        <br>
+        <span class="orignal price">${item.original_price}</span>
+        <br>
+        <span class="Discount">${item.discount_percentage}% Discount price </span>
+    </div>
+    <div class="btn-add-bag ">Add to bag</div>
 
 
-let item = {
-    item_image : 'images/1.jpg',
-
-    rating:{
-        star:4.5,
-        noOfReviews:  1400,
-    },
-    company_name:'Carlton London',
-    item_name:'Rhodium-plated CZ Floral studs',
-    current_price :606,
-    original_price:1045,
-    discount_price:42,
-
-}
+   </div>
+`
+})
 
 
-itemcontainerElement.innerHTML =`
-
-<div class="items-container">
-           <div class="item-container">
-            <img class ="item-image" src="${item.item_image}" alt="image one">
-            <div class="rating">
-               ${item.rating}
-            </div>
-            <div class="company-name">${item.company_name }n</div>
-            <div class="item-name">${item.item_name}</div>
-            <div class="price">
-                <span class="current-price">${item.current_price}</span>
-                <span class="orignal price">${item.original_price}</span>
-                <span class="Discount">{item.discount_price}</span>
-            </div>
-            <div class="btn-add-bag ">Add to bag</div>
 
 
-           </div>`
+itemcontainerElement.innerHTML = innerHTML
